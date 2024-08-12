c8e4dea88aeb02ec8586e8849c54305e7ba2cd82
Vlad <invitegoogl@gmail.com>
2024-08-12 14:45:07 +0300

    api split. Add new urls.py for bot app

---
+++ bot/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from bot.views import LibPageAPI
+
+# апи для страницы библиотеки
+extra_lib_patterns = [
+    path('list_question/<int:page_id>', LibPageAPI.as_view({'get': 'retrieve'})),
+    path('form/', LibPageAPI.as_view({'post': 'create'})),
+    path('predict_answer/', LibPageAPI.as_view({'post': 'get_response'})),
+]
+
+# основной патерн включающие другие патерны апи для страниц
+urlpatterns = [
+    path('lib/', include(extra_lib_patterns))
+]
--- widget/urls.py
+++ widget/urls.py
@@ -15,13 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from bot.views import LibPageAPI
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/list_question/<int:page_id>', LibPageAPI.as_view({'get': 'retrieve'})),
-    path('api/form', LibPageAPI.as_view({'post': 'create'})),
-    path('api/predict_answer', LibPageAPI.as_view({'post': 'get_response'})),
+    path('api/', include('bot.urls', namespace='bot'))
 ]
